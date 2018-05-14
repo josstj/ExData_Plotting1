3ead5091d2a2caed35aaa11a4cfbe9df5d9d9e5d
josstj <36945648+josstj@users.noreply.github.com>
2018-05-13 17:31:25 -0700

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,29 @@
+
+library(data.table)
+library(dplyr)
+
+######### Download data, unzip it, and retrieve tables from main folder
+
+#hide or unhide the download file and unzip commands as needed, files download and unzip to working directory.
+
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","exdata%2Fdata%2Fhousehold_power_consumption.zip")
+unzip("exdata%2Fdata%2Fhousehold_power_consumption.zip")
+
+# Read in data
+data <- tbl_df(fread("household_power_consumption.txt",na.strings="?"))
+
+data$date_time <- as.POSIXct(paste(data$Date,data$Time), format = "%d/%m/%Y %H:%M:%S ")
+
+data$Date <- as.Date(data$Date,"%d/%m/%Y")
+#data$Time <- strptime(data$Time,format = "%H:%M:%S")
+
+sub_data <- filter(data,Date == "2007-02-01" | Date == "2007-02-02")
+
+#plot1
+#hist(sub_data$Global_active_power,col="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+
+#plot 2
+
+png("plot2.png",width = 480,height = 480)
+plot(x = sub_data$date_time,y= sub_data$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,41 @@
+library(data.table)
+library(dplyr)
+
+######### Download data, unzip it, and retrieve tables from main folder
+
+#hide or unhide the download file and unzip commands as needed, files download and unzip to working directory.
+
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","exdata%2Fdata%2Fhousehold_power_consumption.zip")
+unzip("exdata%2Fdata%2Fhousehold_power_consumption.zip")
+
+# Read in data
+data <- tbl_df(fread("household_power_consumption.txt",na.strings="?"))
+
+data$date_time <- as.POSIXct(paste(data$Date,data$Time), format = "%d/%m/%Y %H:%M:%S ")
+
+data$Date <- as.Date(data$Date,"%d/%m/%Y")
+#data$Time <- strptime(data$Time,format = "%H:%M:%S")
+
+sub_data <- filter(data,Date == "2007-02-01" | Date == "2007-02-02")
+
+
+# create panel
+png("plot4.png",width = 480,height = 480)
+
+par(mfrow=c(2,2))
+
+#plot upper left
+plot(x = sub_data$date_time,y= sub_data$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+
+#plot upper right
+plot(x = sub_data$date_time,y= sub_data$Voltage, type = "l", ylab = "Voltage", xlab = "datetime")
+
+#plot lower left
+plot(x = sub_data$date_time,y= sub_data$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
+lines(x=sub_data$date_time,y= sub_data$Sub_metering_2,type = "l", col = "red")
+lines(x=sub_data$date_time,y= sub_data$Sub_metering_3,type = "l", col = "blue")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"),y.intersp = 1)
+
+#plot lower right
+plot(x = sub_data$date_time,y= sub_data$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
+dev.off()
